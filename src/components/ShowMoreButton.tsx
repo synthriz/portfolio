498e4465a092fffa6c81f4a4c7512804722dd15b
triz <111395659+synthriz@users.noreply.github.com>
2026-02-26 00:28:26 -0300

    fix: update ShowMoreButton styles for improved visibility and consistency

--- src/components/ShowMoreButton.tsx
+++ src/components/ShowMoreButton.tsx
@@ -31,7 +31,7 @@ export default function ShowMoreButton({
       className={twMerge(
         "w-full h-14 md:h-16",
         "border-y-2 border-bea-black",
-        "bg-bea-purple text-bea-black",
+        "bg-bea-black/20 text-bea-black dark:text-white dark:bg-bea-white/20",
         "uppercase text-sm md:text-base",
         "transition-colors duration-200 hover:bg-bea-green",
         className
